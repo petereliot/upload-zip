fa11fad93e27e7703ef5d0d92c75d67ee11c17a8
jdegryse <degryse.josiane@gmail.com>
2018-01-11 14:59:17 +0100

    dezipp-test

--- include/upload-file.php
+++ include/upload-file.php
@@ -1,6 +1,23 @@
 <?php
 define("IMG_DIR","../thumbnails");
 
+// vide le dosseir image avant tout
+$dossier_traite = "IMG_DIR";
+$repertoire = opendir($dossier_traite); // On définit le répertoire dans lequel on souhaite travailler.
+while (false !== ($fichier = readdir($repertoire))) {// On lit chaque fichier du répertoire dans la boucle.
+    $chemin = $dossier_traite."/".$fichier; // On définit le chemin du fichier à effacer.
+    // Si le fichier n'est pas un répertoire…
+    if ($fichier != ".." AND $fichier != "." AND !is_dir($fichier))
+    {
+        unlink($chemin); // On efface.
+    }
+}
+closedir($repertoire); // Ne pas oublier de fermer le dossier ***EN DEHORS de la boucle*** ! Ce qui évitera à PHP beaucoup de calculs et des problèmes liés à l'ouverture du dossier.
+
+/*
+ *
+ */
+
 var_dump($_FILES);
 
 // if all went fine ===>
@@ -59,11 +76,23 @@ echo " upload ok ";
                 var_dump($fileinfo);
                 $from = "\n zip://" . $path . "#" . $filename;
                 $to = "\n".IMG_DIR."/" . $filename;
-               // copy("zip://" . $path . "#" . $filename, IMG_DIR."/" . $filename);
+                //copy("zip://".$filename, IMG_DIR."/" . fileinfo["basename"]);
                 $aFileImages[] = $filename;
             }
         }
-
+        /*
+         *  filename = pics/alexwende_4.gif
+         fileinfo = array(4) {
+          ["dirname"]=>
+          string(4) "pics"
+          ["basename"]=>
+          string(15) "alexwende_4.gif"
+          ["extension"]=>
+          string(3) "gif"
+          ["filename"]=>
+          string(11) "alexwende_4"
+        }
+         */
         //$aFileImages[] = $filename;
 
 
